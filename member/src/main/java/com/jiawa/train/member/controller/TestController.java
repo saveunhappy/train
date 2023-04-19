1cadd0275c7f92f374453d1a8514efc0282f8706
JiangTaoHou <925147954@qq.com>
2023-04-19 23:17:59 +0800

    新增member会员模块

--- member/src/main/java/com/jiawa/train/member/controller/TestController.java
+++ member/src/main/java/com/jiawa/train/member/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.jiawa.train;
+package com.jiawa.train.member.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @RequestMapping("hello")
     public String hello(){
-        return "hello world!!!";
+        return "hello world!!!!!!!!!";
     }
 }
