b0f5d2d6ef60d618d9663752242875c5c0900281
JiangTaoHou <925147954@qq.com>
2023-04-20 20:26:18 +0800

    4.6增加http测试文件，方便直接在IDEA中测试

--- member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
+++ member/src/main/java/com/jiawa/train/member/config/MemberApplication.java
@@ -1,15 +1,21 @@
 package com.jiawa.train.member.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.jiawa")
 public class MemberApplication {
-
+	private static final Logger logger = LoggerFactory.getLogger(MemberApplication.class);
 	public static void main(String[] args) {
-		SpringApplication.run(MemberApplication.class, args);
+		SpringApplication app = new SpringApplication(MemberApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		logger.info("启动成功！！");
+		logger.info("测试地址: \thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
 	}
 
 }
