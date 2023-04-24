3a4b3d1015847ef69c41d18fd536fa8c429e98b2
JiangTaoHou <925147954@qq.com>
2023-04-24 21:23:33 +0800

    5.8 增加短信验证码登录接口，升级hutool，支持列表复制

---
+++ member/src/main/java/com/jiawa/train/member/resp/MemberLoginResp.java
@@ -0,0 +1,34 @@
+package com.jiawa.train.member.resp;
+
+public class MemberLoginResp {
+    private Long id;
+
+    private String mobile;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    @Override
+    public String toString() {
+        String sb = getClass().getSimpleName() +
+                " [" +
+                "Hash = " + hashCode() +
+                ", id=" + id +
+                ", mobile=" + mobile +
+                "]";
+        return sb;
+    }
+}
