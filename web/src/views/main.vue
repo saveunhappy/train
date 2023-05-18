4b22d60896ace0044e15e4940c8498c19b0900ac
JiangTaoHou <925147954@qq.com>
2023-05-18 21:33:17 +0800

    6.10 增加路由登录拦截，访问所有的控台页面都需要登录

--- web/src/views/main.vue
+++ web/src/views/main.vue
@@ -31,14 +31,14 @@ export default defineComponent({
     },
     setup() {
         const count = ref(0);
-        axios.get("/member/member/count").then((response) => {
-            let data = response.data;
-            if (data.success) {
-                count.value = data.content;
-            } else {
-                notification.error({ description: data.message });
-            }
-        });
+        // axios.get("/member/member/count").then((response) => {
+        //     let data = response.data;
+        //     if (data.success) {
+        //         count.value = data.content;
+        //     } else {
+        //         notification.error({ description: data.message });
+        //     }
+        // });
         return {
             count
         };
