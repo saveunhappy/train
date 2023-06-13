d4e5c888e3d24fa3ddd2ecbe2ef3ddfc50835cb2
JiangTaoHou <925147954@qq.com>
2023-06-13 22:17:28 +0800

    8.11 修改前端枚举，将key/value改成code/desc，保持和后端一致

--- generator/src/main/java/com/jiawa/train/generator/test/Test1.java
+++ generator/src/main/java/com/jiawa/train/generator/test/Test1.java
@@ -1,6 +1,15 @@
 package com.jiawa.train.generator.test;
 
+import java.util.StringJoiner;
+
 public class Test1 {
 
     private String name;
+
+    public static void main(String[] args) {
+        StringJoiner sj = new StringJoiner(":", "[", "]");
+        sj.add("George").add("Sally").add("Fred");
+        String desiredString = sj.toString();
+        System.out.println(desiredString);
+    }
 }
