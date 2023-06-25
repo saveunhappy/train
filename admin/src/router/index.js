84849dd19ba94fc11b9456deeb2e33d589a82c4a
JiangTaoHou <925147954@qq.com>
2023-06-25 22:52:22 +0800

    9.9 增加火车车箱表，生成持久层代码及前后端代码

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -15,10 +15,16 @@ const routes = [{
   }, {
     path: 'train',
     component: () => import('../views/main/train.vue'),
-  }, {
+  }
+  , {
     path: 'train-station',
     component: () => import('../views/main/train-station.vue'),
-  }]
+  }
+  , {
+    path: 'train-carriage',
+    component: () => import('../views/main/train-carriage.vue'),
+  }
+  ]
 }, {
   path: '',
   redirect: '/welcome'
