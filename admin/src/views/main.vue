88a32a6ade50cbacf09035705f01c7680ede1360
JiangTaoHou <925147954@qq.com>
2023-06-25 19:43:41 +0800

    9.3 去掉没用的HelloWorld组件，修改logo区域

--- admin/src/views/main.vue
+++ admin/src/views/main.vue
@@ -1,15 +1,15 @@
 <template>
-  <a-layout id="components-layout-demo-top-side-2">
-    <the-header-view></the-header-view>
-    <a-layout>
-      <the-sider-view></the-sider-view>
-      <a-layout-content
-          :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
-      >
-        <router-view></router-view>
-      </a-layout-content>
+    <a-layout id="components-layout-demo-top-side-2">
+        <the-header-view></the-header-view>
+        <a-layout>
+            <the-sider-view></the-sider-view>
+            <a-layout-content
+                    :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+            >
+                <router-view></router-view>
+            </a-layout-content>
+        </a-layout>
     </a-layout>
-  </a-layout>
 </template>
 <script>
 import { defineComponent } from 'vue';
@@ -17,31 +17,16 @@ import TheHeaderView from "@/components/the-header";
 import TheSiderView from "@/components/the-sider";
 
 export default defineComponent({
-  components: {
-    TheSiderView,
-    TheHeaderView,
-  },
-  setup() {
-    return {
-    };
-  },
+    components: {
+        TheSiderView,
+        TheHeaderView,
+    },
+    setup() {
+        return {
+        };
+    },
 });
 </script>
 <style>
-#components-layout-demo-top-side-2 .logo {
-  float: left;
-  width: 120px;
-  height: 31px;
-  margin: 16px 24px 16px 0;
-  background: rgba(255, 255, 255, 0.3);
-}
 
-.ant-row-rtl #components-layout-demo-top-side-2 .logo {
-  float: right;
-  margin: 16px 0 16px 24px;
-}
-
-.site-layout-background {
-  background: #fff;
-}
 </style>
