9b5092c4daebe47499dc46843a7c94cb06a3fa9c
JiangTaoHou <925147954@qq.com>
2023-06-26 19:16:47 +0800

    9.12 火车车次下拉选择功能开发，增加后端接口

--- business/src/main/java/com/jiawa/train/business/controller/admin/TrainAdminController.java
+++ business/src/main/java/com/jiawa/train/business/controller/admin/TrainAdminController.java
@@ -11,6 +11,8 @@ import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/admin/train")
 public class TrainAdminController {
@@ -36,4 +38,9 @@ public class TrainAdminController {
         return new CommonResp<>();
     }
 
+    @GetMapping("/query-all")
+    public CommonResp<List<TrainQueryResp>> queryList() {
+        List<TrainQueryResp> list = trainService.queryAll();
+        return new CommonResp<>(list);
+    }
 }
